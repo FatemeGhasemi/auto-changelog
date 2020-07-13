e7e54764d2b3aa1fee2a9b2bd5e6e6ab724689be
renjer <mranjbar.z2993@gmail.com>
2020-07-14 00:29:52 +0430

    Add log

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import 'core-js/stable'
 import run from './run'
 
 export const generateChangelog = async (output, tagPattern = '') => {
-  const newArgs =  process.argv.concat(['--output', output, '--tag-pattern', tagPattern])
+  const newArgs = process.argv.concat(['--output', output, '--tag-pattern', tagPattern])
 
-  console.log('process.argv', {oldArgs :process.argv, newArgs})
+  console.log('process.argv', { oldArgs: process.argv, newArgs })
 
   if (!output) {
     throw new Error('output should be a valid path')
