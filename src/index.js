9779b00edac427e4384948de8b96b22333ee9989
renjer <mranjbar.z2993@gmail.com>
2020-07-13 20:18:43 +0430

    make generateChangelog async

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@
 import 'core-js/stable'
 import run from './run'
 
-export const generateChangelog = (output, tagPattern = '') => {
+export const generateChangelog = async (output, tagPattern = '') => {
   console.log('process.argv', process.argv)
 
   if (!output) {
     throw new Error('output should be a valid path')
   }
   console.log('generateChangelog called', { output, tagPattern })
-  run(process.argv.concat(['--output', output, '--tag-pattern', tagPattern]))
+  return run(process.argv.concat(['--output', output, '--tag-pattern', tagPattern]))
 }
